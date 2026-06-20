f48f30d46ed8973e933b095a5611fab3707140e7
Shashank khatri <shashankkhatri01@gmail.com>
2026-06-20 15:51:56 +0530

    Initial commit

--- API/GMCHPatientImagesDtos/GMCHPatientImagesDtos/obj/Release/net5.0/GMCHPatientImagesDtos.AssemblyInfo.cs
+++ API/GMCHPatientImagesDtos/GMCHPatientImagesDtos/obj/Release/net5.0/GMCHPatientImagesDtos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConfigurationDtos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ce7532e781382e2ffebc3d3341f0f8eb4d1a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConfigurationDtos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConfigurationDtos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- API/GMCHPatientImagesFramework/GMCHPatientImagesFramework/obj/Debug/net5.0/GMCHPatientImagesFramework.AssemblyInfo.cs
+++ API/GMCHPatientImagesFramework/GMCHPatientImagesFramework/obj/Debug/net5.0/GMCHPatientImagesFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConfigurationFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ce7532e781382e2ffebc3d3341f0f8eb4d1a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConfigurationFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConfigurationFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
